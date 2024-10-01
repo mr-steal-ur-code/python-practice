5fead89d086f329dfb2bfc571a1db0b442c97f33
mr-steal-ur-code <cjkarkut@gmail.com>
2024-10-01 04:53:20 -0500

    add sortAndMap example

---
+++ sortAndMapDict.py
@@ -0,0 +1,21 @@
+def send_to_store(cart, aisle_mapping):
+    """Combine users order to aisle and refrigeration information.
+
+    :param cart: dict - users shopping cart dictionary.
+    :param aisle_mapping: dict - aisle and refrigeration information dictionary.
+    :return: dict - fulfillment dictionary ready to send to store.
+    """
+    fulfillment_cart = {}
+    cart_list = sorted(cart.items(), reverse=True)
+    print(f"cart items: {cart.items()}")
+    print(f"cart_list: {cart_list}")
+    for item, quantity in cart_list:
+        if item in aisle_mapping:
+            aisle_info = aisle_mapping[item]
+            aisle_info.insert(0, quantity)
+            fulfillment_cart[item] = aisle_info
+    return fulfillment_cart
+
+
+print(send_to_store({'Banana': 3, 'Apple': 2, 'Orange': 1, 'Milk': 2},
+                    {'Banana': ['Aisle 5', False], 'Apple': ['Aisle 4', False], 'Orange': ['Aisle 4', False], 'Milk': ['Aisle 2', True]}))
